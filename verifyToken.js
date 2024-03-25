83c75e68087fc46b31f4c83abddab328aa5f8315
endocrine <117483689+EndocrineMSc@users.noreply.github.com>
2024-03-25 10:33:48 +0100

    feat: Timed-out accessToken verifications will now send back a new access Token, if a valid refresh token is supplied in the req header

--- verifyToken.js
+++ verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-const tokenRefresh = require("./controllers/tokenRefreshController");
+const RefreshToken = require("./models/refreshToken");
 
 // Verify Token
 exports.verifyToken = asyncHandler(async (req, res, next) => {
@@ -12,17 +12,28 @@ exports.verifyToken = asyncHandler(async (req, res, next) => {
 
   if (!accessToken) return res.sendStatus(403);
 
-  jwt.verify(accessToken, process.env.ACCESS_TOKEN, (err, user) => {
-    if (err) return;
-    req.user = user;
-    next();
+  jwt.verify(accessToken, process.env.ACCESS_TOKEN, async (err, user) => {
+    if (err) {
+      if (!refreshToken) return null;
+
+      if (!(await RefreshToken.findOne({ refreshToken: refreshToken })))
+        return null;
+
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+        if (err) return null;
+
+        const { _id, username } = user;
+        const truncUser = { _id, username };
+
+        const newAccessToken = jwt.sign(truncUser, process.env.ACCESS_TOKEN, {
+          expiresIn: "10m",
+        });
+
+        res.status(401).json(newAccessToken);
+      });
+    } else {
+      req.user = user;
+      next();
+    }
   });
-
-  if (!refreshToken) return res.sendStatus(403);
-
-  const newAccessToken = tokenRefresh.quick_refresh(refreshToken);
-
-  if (!newAccessToken) return res.sendStatus(403);
-
-  res.status(401).json(newAccessToken);
 });
