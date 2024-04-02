0edb6fc63f5facb714e5d33600175e5ade63b629
endocrine <117483689+EndocrineMSc@users.noreply.github.com>
2024-04-02 18:43:50 +0200

    feat: Enable cors for api requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 
@@ -23,6 +24,7 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
